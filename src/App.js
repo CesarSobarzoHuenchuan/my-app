b98d75a4873fa375902dce028e5ca0eef1f54039
[CesarSobarzoHuenchuan] <[cesarignacio.sobarzo@alumnos.ulagos.cl]>
2022-12-12 20:26:57 -0300

    Prueba 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState, useEffect} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
+  const [data, setData]=useState(null)
+
+  useEffect(()=>{
+      getData()
+  }, [])
+
+  const getData = async () => {
+    try{
+      const request = await fetch('https://dummyjson.com/products')
+      const response = request.ok ? await request.json() : await request.text();
+      setData(response)
+      console.log(data)
+    }
+    catch(error){
+      console.error(error)
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Phones</h1>
+      {data && data.products.map((item) => (
+        <div>
+          <Container>
+            <Row>
+            <ListGroup>
+              <ListGroup.Item as="li" active key={item.title}>{item.title}</ListGroup.Item>
+              <ListGroup.Item key={item.description}>{item.description}</ListGroup.Item>
+              <ListGroup.Item key={item.price}>{item.price}</ListGroup.Item>
+              <ListGroup.Item></ListGroup.Item>
+            </ListGroup>
+            <img src={item.thumbnail}></img>
+            </Row>
+          </Container>
+
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+/*
+
+  return (
+    <div>
+      <h1>Phones</h1>
+      {data && data.products.map((item) => (
+        <div>
+          <li key={item.title}>{item.title}</li>
+          <li key={item.description}>{item.description}</li>
+          <li key={item.price}>{item.price}</li>
+          <img src={item.thumbnail}></img>
+        </div>
+      ))}
+    </div>
+  );
+
+
+
+*/
